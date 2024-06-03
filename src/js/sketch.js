57e828c77ac9512f2d4c008959e74e123d5665cf
jeonghye park <byulnim2002@gmail.com>
2024-06-04 00:25:26 +0900

    p5.js 인스턴스화

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,8 +1,16 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let sketch = function(p) {
+  let x = 100;
+  let y = 100;
 
-function draw() {
-  background(220);
-  ellipse(50, 50, 50, 50);
-}
+  p.setup = function() {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+
+  p.draw = function() {
+    p.background(0);
+    p.fill(255);
+    p.rect(x, y, 50, 50);
+  };
+};
+
+let myp5 = new p5(sketch);
